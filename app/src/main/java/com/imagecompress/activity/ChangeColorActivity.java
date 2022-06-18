f9b595880f7a5443a96165e91cee41fef7ba1d8b
Bill-Haku <battleandwar@hotmail.com>
2022-06-18 13:29:06 +0800

    feat: save image when changing color

--- app/src/main/java/com/imagecompress/activity/ChangeColorActivity.java
+++ app/src/main/java/com/imagecompress/activity/ChangeColorActivity.java
@@ -6,11 +6,13 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.SeekBar;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -18,15 +20,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.imagecompress.R;
 import com.imagecompress.utils.ImageHelper;
 
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.net.URI;
+import java.util.Random;
 //https://blog.csdn.net/cfy137000/article/details/54646912
 
 public class ChangeColorActivity extends AppCompatActivity implements SeekBar.OnSeekBarChangeListener {
     private ImageView mChangeColorIv;
     private SeekBar mHueSeekBar, mSaturationSeekBar, mLumSeekBar;
-    private Button mChooseButton;
+    private Button mChooseButton, mSaveButton;
 
-    private Bitmap mBitmap;
+    private Bitmap mBitmap = null;
 
     private float mHue = 0, mSaturation = 1f, mLum = 1f;
     private static final int MID_VALUE = 128;
@@ -40,7 +47,44 @@ public class ChangeColorActivity extends AppCompatActivity implements SeekBar.On
         mHueSeekBar = (SeekBar) findViewById(R.id.hue_seek_bar);
         mSaturationSeekBar = (SeekBar) findViewById(R.id.saturation_seek_bar);
         mLumSeekBar = (SeekBar) findViewById(R.id.lum_seek_bar);
-        onClickButtonListener();
+        mChooseButton = (Button) findViewById(R.id.choose_btn);
+        mChooseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_PICK, null);
+                intent.setDataAndType(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, "image/*");
+                startActivityForResult(intent, 2);
+            }
+        });
+        mSaveButton = (Button) findViewById(R.id.save_btn);
+        mSaveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mBitmap != null) {
+                    mChangeColorIv.buildDrawingCache(true);
+                    mChangeColorIv.buildDrawingCache();
+                    Bitmap saveBitmap = mChangeColorIv.getDrawingCache();
+                    File temp = new File("/sdcard/DCIM/image");
+                    if (!temp.exists()) {
+                        temp.mkdirs();
+                    }
+                    Random random = new Random();
+                    int randNum = random.nextInt(99999999);
+                    String randStr = String.valueOf(randNum);
+                    String fileName = Environment.getExternalStorageDirectory().getPath() +  "/DCIM/magicimage/" +  randStr + ".jpg";
+                    File file = new File(fileName);
+                    try {
+                        BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(file));
+                        saveBitmap.compress(Bitmap.CompressFormat.JPEG, 100, bos);
+                        bos.flush();
+                        bos.close();
+                        Toast.makeText(getApplicationContext(), "图片已保存到[ExternalStorageDirectory]/DCIM/magicimage/", Toast.LENGTH_SHORT).show();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
 
         //获得图片资源
 //        mBitmap = BitmapFactory.decodeResource(getResources(), R.mipmap.ayaki);
@@ -51,18 +95,6 @@ public class ChangeColorActivity extends AppCompatActivity implements SeekBar.On
         mSaturationSeekBar.setOnSeekBarChangeListener(this);
         mLumSeekBar.setOnSeekBarChangeListener(this);
 
-    }
-
-    public void onClickButtonListener() {
-        mChooseButton = (Button) findViewById(R.id.choose_btn);
-        mChooseButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(Intent.ACTION_PICK, null);
-                intent.setDataAndType(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, "image/*");
-                startActivityForResult(intent, 2);
-            }
-        });
     }
 
     @Override
@@ -96,9 +128,10 @@ public class ChangeColorActivity extends AppCompatActivity implements SeekBar.On
                 break;
         }
 
-        Bitmap bitmap = ImageHelper.getChangedBitmap(mBitmap,
-                mHue, mSaturation, mLum);
-        mChangeColorIv.setImageBitmap(bitmap);
+        if (mBitmap != null) {
+            Bitmap bitmap = ImageHelper.getChangedBitmap(mBitmap, mHue, mSaturation, mLum);
+            mChangeColorIv.setImageBitmap(bitmap);
+        }
     }
 
     @Override
